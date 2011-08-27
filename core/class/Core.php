4c603f0f7ac48269386a32760ba7d34865e2844c
Shafiul Azam <ishafiul@gmail.com>
2011-08-27 23:31:43 +0600

    Core & CoreForm documented

--- core/class/Core.php
+++ core/class/Core.php
@@ -23,27 +23,43 @@ require_once dirname(__FILE__) . "/../funcs/general.php";
 require_once dirname(__FILE__) . "/../../" . CUSTOM_DIR . "/class/CustomView.php";
 require_once dirname(__FILE__) . "/../../" . CUSTOM_DIR . "/class/Validator.php";
 
+/** \brief The Most important class! 
+ * 
+ * This class contains everything you need. An object of this class, $core (global variable) is created
+ * for you. Within controllers, views & templates, you can get access to this class by this code:
+ * global $core;
+ * In fact, this $core variable is the only thing you need. See it's member variables
+ */
+
 class Core{
-    public $funcs;
-    public $html;
-    public $validate;
-    public $view;
-    public $controller;
-    public $page;   //  Which page we are in?
-    public $functionToCall; //  Which function to call inside Controller
-    public $formData;
+    public $funcs;  ///<    An object of class: Funcs
+    public $html;   ///<    An object of class: HTML
+    public $validate;   ///<    An object of class:  Validator 
+    public $view;   ///<    An object of class: CustomView
+    public $controller; ///<    An object of class: Controller
+    public $page;   ///<    cotains "The Page" - see documentation for more details
+    public $functionToCall; ///<  contains the name of the function of constructor to call.
+    public $formData;   ///<    Key-value Array for containing HTML strings for web forms
     // Others
-    public $theme;
-    private $data;
+    public $theme;  ///<    Name of the template
+    private $data;  ///<    Key-value array for containing variables, which are passed from Controller to View.
     
     // Load sttus
-    public $controllerLoaded = false;
-    public $viewLoaded = false;        
-    public $modelLoaded = false;   // Only for default model
+    public $controllerLoaded = false;   ///<    Boolean, true if controller class loaded.
+    public $viewLoaded = false;         ///<    Boolean, true if view class loaded.        
+    public $modelLoaded = false;        ///<    Boolean, true if "Default" model class loaded. See documentation
     
     // vars for internal use. Don't use/depend on any of these in your code
      
     // constructor
+    
+    /**
+     * Constractor.
+     * - Initializes member variables
+     * - Sets default template (in config/general.php )
+     * @param None
+     * @return None
+     */
     
     public function __construct() {
         $this->html = new HTML();
@@ -56,6 +72,13 @@ class Core{
     
     /* Loaders */
     
+    /**
+     * Use this function to Load a Model class. You can load any number of model classes.
+     * Call this function within your constructor. If not called, the default Model gets loaded automatically.
+     * @param string $model name of the Model class. This class must reside under MODEL directory.
+     * @return bool true in success, false in failure.
+     */
+    
     public function loadModel($model){
         $filename = dirname(__FILE__) . "/../../" . MODEL_DIR . "/$model.php";
         if(file_exists($filename)){
@@ -67,6 +90,13 @@ class Core{
             return false;
         }
     }
+    
+    /**
+     * Use this function to Load a View class. You can load ONLY ONE view class for a "page".
+     * Call this function within your constructor. If not called, the default view gets loaded.
+     * @param string $view name of the View class. This class must extend CustomView & reside under VIEW directory.
+     * @return bool true in success, false in failure.
+     */
     
     public function loadView($view){
         if(!$this->viewLoaded){
@@ -82,6 +112,14 @@ class Core{
         }
     }
     
+    /**
+     * This function loads a Controller class. 
+     * - You should NEVER call this function! As this function is automatically called by the framework.
+     * 
+     * @param string $controller name of the controller class
+     * @return bool
+     */
+    
     public function loadController($controller){
         if(!$this->controllerLoaded){
             $filename = dirname(__FILE__) . "/../../" . CONTROL_DIR . "/$controller.php";
@@ -96,23 +134,146 @@ class Core{
         }
     }
     
+    /**
+     * Call this from your controller to load a "Custom Class"
+     * @param string $className name of the class. This class must reside in CUSTOM/class directory.
+     * @return bool true in success, false otherwise 
+     */
     
     public function loadCustomClass($className){
         $classPath = dirname(__FILE__) . "/../../" . CUSTOM_DIR . "/class/$className.php";
         return $this->safeRequireOnce($classPath);
     }
     
-    public function loadCustomFunction($funcName){
-        $classPath = dirname(__FILE__) . "/../../" . CUSTOM_DIR . "/funcs/$className.php";
+    /**
+     * Call this from your controller to load a "Custom Functions" file
+     * @param string $funcFile name of the file. This file must reside in CUSTOM/funcs directory.
+     * @return bool true in success, false otherwise 
+     */
+    
+    public function loadCustomFunction($funcFile){
+        $classPath = dirname(__FILE__) . "/../../" . CUSTOM_DIR . "/funcs/$funcFile.php";
         return $this->safeRequireOnce($classPath);
     }
+    
+    /**
+     * Call this from your controller to load a Form
+     * @param string $formName name of the class. This class must extend CoreForm & reside in VIEW/forms directory.
+     * @return bool true in success, false otherwise 
+     */
     
     public function loadForm($formName){
         $classPath = dirname(__FILE__) . "/../../" . FORMS_DIR . "/$formName.php";
         return $this->safeRequireOnce($classPath);
     }
     
+    
+    /* Template Related */
+    
+    
+    /**
+     * Call this withing your Controller to set a different template, other than the Default one.
+     * This change will be valid only for the current "page"
+     * - Call this function if you explicitly call loadView($view)
+     * @param string $theme name of the template
+     * @return None
+     */
+    
+    public function setTemplate($theme){
+        $this->theme = $theme;
+    }
+    
+    
+
+
+    // Data passing across controller-view 
+    
+    /**
+     * Use this function within your controller to pass a variable to View.
+     * Within View, you can get the data by calling getData() , $id is the ID of the variable.
+     * @param string $id ID of the variable. You must use the same ID when you call getData() to retrie the variable
+     * @param variable $data The actual variable you want to pass.
+     * @return None 
+     */
+    
+    public function setData($id,$data){
+        $this->data[$id] = $data;
+    }
+    
+    /**
+     * Use this function within your View to retrive the variable you passed within your controller.
+     * @param string $id ID you used when calling setData()
+     * @return variable you passed using setData() - if you provide valid ID
+     * @return bool false - if you provide invalid ID
+     */
+    
+    public function getData($id){
+        if(isset ($this->data[$id]))
+            return $this->data[$id];
+        else
+            return false;
+    }
+    
+    // Form Related
+    
+    /**
+     * Call this function within your View to get html string of a form.
+     * - Before calling this function, within your Controller prepare the form & call CoreForm::sendToView() , see CoreForm documentation 
+     * @param string $formClassName name of the form - this form must exist in VIEW/forms directory.
+     * @return string html data of the form, or an error string if the form was not set within controller.
+     */
+    
+    public function getForm($formClassName){
+        $formClassName = strtolower($formClassName);
+//        print_r($this->formData);
+        return (isset ($this->formData[$formClassName]))?($this->formData[$formClassName]):("Error: $formClassName form not found!");
+    }
+    
+    
+    
+    
+    /**
+     * This function is used to safely load a php file. you can use it instead of require_once()
+     * @param string $filename name of the file to load
+     * @return bool true if file found, false otherwise. 
+     */
+    
+    public function safeRequireOnce($filename){
+        if(file_exists($filename)){
+            require_once $filename;
+            return true;
+        }else{
+            $this->debug ("File $filename Not found!");
+            return false;
+        }
+    }
+    
+    
+    /** @name Functions for Internal Use
+     * These functions are used internally by the framework.
+     * - You should NEVER call these functions! As they are automatically called by the framework.
+     */
+    
+    //@{
+    
+    /**
+     * Echoes the string provided as parameter $str if DEBUG_MODE is set true.
+     * @param string $str the string to echo.
+     */
+    
+    public function debug($str){
+        if(DEBUG_MODE)
+            echo "<br /><pre>$str</pre>";
+    }
+    
     /* Useful functions for index page */
+    
+    /**
+     * Used to load the default model, view & controller.
+     * - You should NEVER call this function! As this function is automatically called by the framework.
+     * @param string $page "The Page"
+     * @return None
+     */
     
     public function loadMVC($page){
         $this->findPage($page);
@@ -124,13 +285,10 @@ class Core{
         
     }
     
-    
-    
-    /* Template Related */
-    
-    public function setTemplate($theme){
-        $this->theme = $theme;
-    }
+    /**
+     * Loads necessary files (mainly index.php) from templates/<SELECTED TEMPLATE> folder.
+     * - You should NEVER call this function! As this function is internally called by the framework. 
+     */
     
     public function loadTemplate(){
         if($this->viewLoaded){
@@ -141,6 +299,10 @@ class Core{
         }
     }
     
+    /**
+     * Creates an object of the View class.
+     * - You should NEVER call this function! As this function is automatically called by the framework. 
+     */
     
     public function generateViewObject(){
         if($this->viewLoaded){
@@ -160,6 +322,11 @@ class Core{
     
     // Controller related
     
+    /**
+     * Creates object of the Controller class. Also calls the function specified by "functionToCall"
+     * - You should NEVER call this function! As this function is automatically called by the framework. 
+     */
+    
     public function generateControllerObject(){
         if($this->controllerLoaded){
             $this->controller = new Controller();
@@ -175,28 +342,15 @@ class Core{
             }
         }
     }
-
-
-    // Data passing across controller-view 
     
-    public function setData($id,$data){
-        $this->data[$id] = $data;
-    }
-    
-    public function getData($id){
-        if(isset ($this->data[$id]))
-            return $this->data[$id];
-        else
-            return false;
-    }
-    
-    // Form Related
-    public function getForm($formClassName){
-        $formClassName = strtolower($formClassName);
-//        print_r($this->formData);
-        return (isset ($this->formData[$formClassName]))?($this->formData[$formClassName]):("Error: $formClassName form not found!");
-    }
     // Utility Functions
+    
+    /**
+     * Used internally to find out the "page" & "functionToCall"
+     * - THIS IS USED INTERNALLY, NEVER CALL THIS FUNCTION! 
+     * @param string $page the query string user provided
+     * @return None 
+     */
     
     public function findPage($page){
         $pageArr  = explode("/", $page);
@@ -224,23 +378,10 @@ class Core{
                 $this->page = implode("/", $pageArr);
             }
         }
-        $this->debug("Page: " . $this->page);
+//        $this->debug("Page: " . $this->page);
     }
     
-    public function debug($str){
-        if(DEBUG_MODE)
-            echo "<br /><pre>$str</pre>";
-    }
-    
-    public function safeRequireOnce($filename){
-        if(file_exists($filename)){
-            require_once $filename;
-            return true;
-        }else{
-            $this->debug ("File $filename Not found!");
-            return false;
-        }
-    }
+    //@}
     
     
 }
--- core/class/CoreForm.php
+++ core/class/CoreForm.php
@@ -15,43 +15,63 @@ define("FORM_RESULT", 0);
 define("FORM_ERROR_STRING", 1);
 define("FORM_HTML",2);
 
+/**
+ * \brief Create, submit & validate web-forms
+ * 
+ * This class provides opportunities for:
+ * - Creating web forms easily, withtout writing any %HTML: within your %Controller
+ * - Send object of a form to %View class
+ * - Process submission of a form, & perform validation
+ * - If validation fails, automatically generate Error messages & re-present form to user, keeping user provided data intact.
+ */
+
 abstract class CoreForm extends HTML {
     
 
-    public $action = '';
-    public $method = 'post';
-    public $target = '';
-    public $onSubmit = '';
-    public $elements = array();
-    public $submitButtonText = '';
-    public $tableBorder = '0';
-    public $tableCellSpacing = '';
-    public $tableCellPadding = '';
-    public $arbritaryHTML = '';
-    public $fileUpload = false;
-    public $id = "";
-    public $submitButtonId = "";
-    public $class = "";
-    public $isShowButton = true;
-    public $displaySubmissionErrors = true;
+    public $action = '';    ///<    for form attribute "action"
+    public $method = 'post';    ///<    for form attribute "method"
+    public $target = '';        ///<    for Form attribute "target"
+    public $onSubmit = '';  ///<    for Form attribute "onsubmit"
+    public $elements = array(); ///<    Key-value array for storing form elements
+    public $submitButtonText = '';  ///<    Text displayed at form submit button
+    public $tableBorder = '0';  ///<    for Table attribute "border"
+    public $tableCellSpacing = '';  ///<    for Table attribute "cellspacing"
+    public $tableCellPadding = '';  ///<    for table attribute "cellpadding"
+    public $arbritaryHTML = ''; ///<    This %HTML string is placed after form elements
+    public $fileUpload = false; ///<    Set true if you're uploading file(s) by this form
+    public $id = "";    ///<    for Form attribute "id"
+    public $submitButtonId = "";    ///<    for Form's submit button's attribute "id"
+    public $class = ""; ///<    for Form attribute "class"
+    public $displaySubmissionErrors = true; ///< Set to true if you want to display error messages when form validation fails
     
-    private $validate = false;
-    private $submittedData = array();
-    private $error = "";
-    private $formHtml = "";
-    private $core = null;
-    private $formName = "";
+    private $validate = false;  ///<    Automatically set to true when you're validating a submitted form
+    private $submittedData = array();   ///< Used to store user-submitted data by this form
+    private $error = "";    ///< Contains error-strings if form validation fails.
+    private $formHtml = ""; ///< Contains the %HTML string generated for this form
+    private $core = null;   ///<    A reference to the global $core
+    private $formName = ""; ///<    Name of the class which extended me ( CoreClass )
 
     // Public & private Methods
     
+    /**
+     * Generally, you extend CoreForm class when you're creating a form class (See example child class: Registration form in VIEW/forms directory)
+     * In your Form class's constructor, you should call this function. (see the example classed mentioned)
+     * @param object $ob should be always "$this" - reference to the child class.
+     * @param Core $core a reference to the global $core 
+     */
+    
     public function __construct($ob,$core) {
         $this->core = $core;
-        $this->formName = strtolower(get_class($ob));
+        $this->formName = get_class($ob);
     }
     
-    abstract public function createElements();  //  To be implemented
-
-
+    /**
+     * Creates & returns %HTML for this form. 
+     * - You do not need to use this function at all if you use sendToView() function. Example: CONTROL/registration.php & VIEW/registration.php
+     * - If you want to manually send the generated %HTML of the form to your view class, you may call this function within your controller. See example: CONTROL/login.php & VIEW/login.php
+     * @return string generated html for this form 
+     */
+    
     public function create() {
         // Creates the HTML form and returns the HTML
         // Create elements
@@ -85,10 +105,22 @@ abstract class CoreForm extends HTML {
         return $this->formHtml;
     }
     
+    /**
+     * Call this function within your controller to automatically send the generated html to your view class.
+     * Within your view class, you can call Core::getForm($id) to gain the html, where $id is the class name of the form
+     */
+    
     public function sendToView(){
-//        echo "sent to view";
         $this->core->formData[$this->formName] = $this->create();
     }
+    
+    /**
+     * Call this function within your controller to start validation of the form.
+     * @return array | You will need to check only the 0th element of the array, if you are using sendToView()
+     * - 0th element of the array: boolean, true if all validation functions successful, false if one or more validation functions failed. 
+     * - 1st element of the array: string, error message if validation failed
+     * - 2nd element of the array: string, re-generated %html of the form, with user submitted values :-)
+     */ 
     
     public function validate(){
         // When form submitted, used this to validate the form.
@@ -96,7 +128,83 @@ abstract class CoreForm extends HTML {
         return $this->create();
     }
     
-    public function doValidation($name){
+    
+    
+    
+    // Element related
+    
+    /**
+     * Call this function within your child form class to set various properties for a form element. See parameters 
+     * - element() & elementHTML() are always called in pair for an element of the form. element() MUST be called BEFORE the corresponding elementHTML() of the pair.
+     * - see Registration or Login class for example. 
+     * @param string $name the "name" attribute of the element
+     * @param string $displayName the label displayed for the element. This value also used for identifying the element in error messages.
+     * @param type $validators a list of validation functions seperated by "|" - see documentations for details. Validation functions are located in CoreValidator (in core/class directory) & Validator (in custom/class directory) classes
+     */
+    
+    public function element($name,$displayName,$validators = ""){
+        $this->elements[$name] = array($displayName, $validators);
+    }
+    
+    /**
+     * Call this function within your child form class to set the %HTML for a form element. 
+     * - element() & elementHTML() are always called in pair for an element of the form. element() MUST be called BEFORE the corresponding elementHTML() of the pair.
+     * - see Registration or Login class for example. 
+     * @param string $name the "name" attribute of the element
+     * @param type $html the html for this element. You can use input(), textarea(), select() etc. functions to create the %HTML 
+     */
+    public function elementHTML($name,$html){
+        $this->elements[$name][2] = $html;
+//        echo "here " . count($this->elements);
+    }
+    
+    /**
+     * Call this function within your constructor to get VALIDATED user-submitted value for a single element.
+     * @param string $name the "name" attribute of the element
+     * @return string | validated user submitted value 
+     */
+    
+    public function get($name){
+        return (isset($this->submittedData[$name]))?($this->submittedData[$name]):(null);
+    }
+    
+    /**
+     * Call this function within your constructor to get VALIDATED user-submitted value for all form elements.
+     * @return array | key-value array where key is the "name" attribute of the element. 
+     */
+    
+    public function getAll(){
+        return $this->submittedData;
+    }
+    
+    /**
+     * If you are using sendToView() call this function within your constructor to automatically
+     * send error message & generated html (with user-provided values) of the form to the view.
+     */
+    
+    public function resubmit(){
+        $this->core->funcs->setDisplayMsg($this->error);
+        $this->core->formData[$this->formName] = $this->formHtml;
+    }
+
+
+    
+    /**
+     * @name Functions for Internal Use
+     * These functions are called automatically within the framework.
+     * You should NEVER call these functions in your code!
+     */
+    
+    
+    //@{
+    
+    /**
+     * Used Internally to start validation of a form element
+     * @param string $name name of the element to start validation
+     * @return string | validated user input for this element 
+     */
+    
+    private function doValidation($name){
         $element = $this->elements[$name];
         $funcsToCall = explode("|", $element[1]);
         // Get the post value to begin examination!
@@ -112,41 +220,30 @@ abstract class CoreForm extends HTML {
     }
     
     
-    // Element related
+    //@}
     
-    public function element($name,$displayName,$validators = ""){
-        $this->elements[$name] = array($displayName, $validators);
-//        echo "here " . count($this->elements);
-    }
+    /**
+     * @name Functions for creating %HTML for form-elements
+     */
     
-    public function elementHTML($name,$html){
-        $this->elements[$name][2] = $html;
-//        echo "here " . count($this->elements);
-    }
     
-    public function get($name){
-        return (isset($this->submittedData[$name]))?($this->submittedData[$name]):(null);
-    }
+    //@{
     
-    public function getAll(){
-        return $this->submittedData;
-    }
-    
-    public function resubmit(){
-        // Resubmit the form
-        $this->core->funcs->setDisplayMsg($this->error);
-        $this->core->formData[$this->formName] = $this->formHtml;
-    }
-
-
-    // Override parent HTML class's functions
-    
+    /**
+     *
+     * See HTML::input() for documenttion
+     */
     public function input($name,$type="text", $id="", $value="", $attrArr = null) {
         if($this->validate){
             $value = $this->doValidation($name);
         }
         return parent::input($name, $type, $id, $value, $attrArr);
     }
+    
+    /**
+     *
+     * See HTML::textarea() for documenttion
+     */
     
     public function textarea($name,$value = "",$attrArr=null, $id= ""){
         if($this->validate){
@@ -155,12 +252,35 @@ abstract class CoreForm extends HTML {
         return parent::textarea($name, $value, $attrArr, $id);
     }
     
+    /**
+     *
+     * See HTML::select() for documenttion
+     */
+    
     public function select($name, $options, $selectedValue = "", $attrArr= null, $id=null) {
         if($this->validate){
             $selectedValue = $this->doValidation($name);
         }
         return parent::select($name, $options, $selectedValue, $attrArr, $id);
     }
+    
+    
+    //@}
+    
+    /**
+     * @name Abstract functions: implement these functions in Child class.
+     */
+    
+    //@{
+    
+    /**
+     * Within this function call a sequence of element() & elementHTML() functions in pair. 
+     * For example, see Registration class
+     */
+    
+    abstract public function createElements();  //  To be implemented
+    //@}
 }
+
 
 ?>
